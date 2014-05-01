08db78336068ed565f0199a63cd2d3cbe805fbde
Todor Pitekov <esembidi@gmail.com>
2014-05-01 22:05:50 +0300

    added incomplete implementation of the game object which will encapsulate game state and will provide run loop

---
+++ source/game/server/game.js
@@ -0,0 +1,27 @@
+var Dispatch = require('../../sra/src/util/dispatch.js');
+
+var Game = function (server, updateRate) {
+	this.server = server;
+	this.updateRate = updateRate;
+	this.runLoop = new Dispatch.RunLoop(updateRate, this.update, this);
+
+	this.fieldSize = {width: 800, height: 600};
+
+	this.runLoop.start();
+};
+
+Game.prototype.update = function (context) {
+	var self = context;
+	self.server.updateClients();
+	self.server.updateActors();
+};
+
+Game.prototype.toMessage = function () {
+	return {
+		w: this.fieldSize.width,
+		h: this.fieldSize.height,
+		ur: this.updateRate
+	};
+};
+
+module.exports = Game;
