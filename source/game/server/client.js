31d890b55c93f86c66d5914b99816751c7e394d6
Todor Pitekov <esembidi@gmail.com>
2014-05-01 22:04:33 +0300

    added incomplete implementation of a client

---
+++ source/game/server/client.js
@@ -0,0 +1,39 @@
+var BISON = require('bison');
+var Message = require('../message.js');
+var Actor = require('./actor.js');
+
+var Client = function (server, connection, clientID) {
+	this.id = clientID;
+	this.server = server;
+	this.conn = connection;
+	this.name = null;
+	this.player = null;
+
+	this.initiated = false;
+};
+
+Client.prototype.update = function () {
+
+}
+
+Client.prototype.handleMessage = function (message) {
+	switch (message.type) {
+		case Message.NAME:
+			if (!this.name && message.name) {
+				this.name = message.name;
+				this.player = this.server.addActor(Actor.PlayerActor, {client: this});
+			}
+			break;
+
+		default:
+			break;
+	}
+
+	return true;
+};
+
+Client.prototype.sendMessage = function (message) {
+	this.conn.send(BISON.encode(message));
+};
+
+module.exports = Client;
