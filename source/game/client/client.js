6ef551b552f4dee45b2083477a745a78a76f4aea
Todor Pitekov <esembidi@gmail.com>
2014-05-03 00:05:52 +0300

    added client - it can connect to the server, send and receive messages and handles connection-related events

--- source/game/client/client.js
+++ source/game/client/client.js
@@ -1,16 +1,104 @@
+var Client = function () {
+	this.sock = null;
+	this.connected = false;
+	this.error = false;
+	this.controller = null;
+	this.playing = false;
+}
 
-var ws = new WebSocket('ws://' + Config.host + ':' + Config.port);
+Client.prototype.connect = function (host, port) {
+	if (this.connected) {
+		return;
+	}
 
-ws.onopen = function () {
-	console.log('opened');
-	ws.send(BISON.encode({type:Message.INIT}));
-	ws.send(BISON.encode({type:Message.NAME, name:'mordek'}));
-};
+	this.sock = new WebSocket('ws://' + Config.host + ':' + Config.port);
+	var self = this;
 
-ws.onmessage = function(msg) {
-	console.log('received: ' + JSON.stringify(BISON.decode(msg.data)));
-};
+	this.sock.onopen = function () {
+		console.log('connected');
+		self.connected = true;
+		self.error = false;
+		self.handleConnect();
+	}
 
-ws.onerror = ws.onclose = function(e) {
-    console.log('closed ' + e);
-};
+	this.sock.onmessage = function (event) {
+		var msg;
+
+		try {
+			msg = BISON.decode(event.data);
+		} catch (e) {
+			console.log('message parse exception: ' + e);
+		}
+		
+		if (!msg || !msg.type) {
+			console.log('invalid message format, closing connection');
+			self.error = true;
+			self.sock.close();
+		} else {
+			self.handleMessage(msg);
+		}
+	}
+
+	this.sock.onclose = function (event) {
+		console.log('connection closed: ' + event);
+		self.connected = false;
+		self.sock = null;
+		self.handleDisconnect(self.error);
+	}
+
+	this.sock.onerror = function (event) {
+		console.log('connection error: ' + event);
+		self.error = true;
+		// no need to modify client state here as the 'onclose' handler is automatically invoked after this one
+	}
+}
+
+Client.prototype.disconnect = function () {
+	if (!this.connected) {
+		return;
+	}
+
+	this.sock.close();
+	this.sock = null;
+}
+
+Client.prototype.handleConnect = function () {
+	this.sendMessage(Message.buildInitMessage());
+
+	var self = this;
+
+	showNameEntryDialogue(function (text) {
+		hideNameEntryDialogue();
+
+		if (!self.connected) {
+			return;
+		}
+
+		self.sendMessage(Message.buildNameMessage(text));
+	});
+}
+
+Client.prototype.handleDisconnect = function (error) {
+
+}
+
+Client.prototype.handleMessage = function (message) {
+	console.log('handle ' + JSON.stringify(message));
+	switch (message.type) {
+		case Message.GAME_DATA:
+			this.controller.initializeWithGameData(message.data);
+			break;
+
+		case Message.GAME_START:
+			this.playing = true;
+			break;
+
+		case Message.ACTOR_ADD:
+			this.controller.addActor(message.data);
+			break;
+	}
+}
+
+Client.prototype.sendMessage = function (message) {
+	this.sock.send(BISON.encode(message));
+}
