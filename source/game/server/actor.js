aca4e5a134764d5a6d15a1aac1a1bcc21a39918c
Todor Pitekov <esembidi@gmail.com>
2014-05-02 23:59:17 +0300

    server actor now has id, position and size which are also serialized to the actor message; added test values for the position and size

--- source/game/server/actor.js
+++ source/game/server/actor.js
@@ -5,13 +5,15 @@ var Types = {
 };
 
 var Actor = {
-	_init: function (type, client) {
+	_init: function (id, type, client) {
+		this.id = id;
 		this.type = type;
 		this.client = client;
 		this.initiated = false;
 		this.updated = false;
 
-		this.position = Geometry.Vector2.Zero.clone();
+		// this.position = Geometry.Vector2.Zero.clone();
+		this.position = new Geometry.Vector2(100.0, 100.0);
 	},
 
 	toMessage: function () {
@@ -19,21 +21,28 @@ var Actor = {
 	}
 };
 
-var PlayerActor = function (data) {
-	this._init(Types.PLAYER, data.client);
+var PlayerActor = function (id, data) {
+	this._init(id, Types.PLAYER, data.client);
 
 	this.velocity = 0;
 	this.rotation = 0;
+
+	this.width = 50.0;
+	this.height = 50.0;
 };
 
 PlayerActor.prototype = Object.create(Actor);
 
 PlayerActor.prototype.toMessage = function () {
 	return {
+		id: this.id,
+		t: this.type,
 		x: this.position.x,
 		y: this.position.y,
+		w: this.width,
+		h: this.height,
 		v: this.velocity,
-		r: this.rotation
+		r: this.rotation,
 	};
 };
 
