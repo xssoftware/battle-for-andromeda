2f2d114781059719d021f7e352c22bcc631342d6
Todor Pitekov <esembidi@gmail.com>
2014-05-02 12:01:35 +0300

    added message type for actor update, added message builder

--- source/game/message.js
+++ source/game/message.js
@@ -5,20 +5,25 @@
 	exports.GAME_DATA = 16;
 
 	exports.ACTOR_ADD = 32;
+	exports.ACTOR_UPDATE = 33;
 
 	exports.buildInitMessage = function () {
 		return {type: exports.INIT};
-	}
+	};
 
 	exports.buildNameMessage = function (name) {
 		return {type: exports.NAME, name: name};
-	}
+	};
 
 	exports.buildGameDataMessage = function (data) {
 		return {type: exports.GAME_DATA, data: data};
-	}
+	};
 
 	exports.buildActorAddMessage = function (data) {
 		return {type: exports.ACTOR_ADD, data: data};
-	}
+	};
+
+	exports.buildActorUpdateMessage = function (data) {
+		return {type: exports.ACTOR_UPDATE, data: data};
+	};
 })(typeof exports === 'undefined' ? this.Message = {} : exports);
