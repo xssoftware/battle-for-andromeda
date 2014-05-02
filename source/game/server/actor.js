b2421acfce66e5a95dd55c8e5a635f23d97aa799
Todor Pitekov <esembidi@gmail.com>
2014-05-02 12:02:02 +0300

    server actors now have a position associated with them

--- source/game/server/actor.js
+++ source/game/server/actor.js
@@ -1,3 +1,5 @@
+var Geometry = require('../../sra/src/util/geometry.js');
+
 var Types = {
 	PLAYER: 1
 };
@@ -8,6 +10,8 @@ var Actor = {
 		this.client = client;
 		this.initiated = false;
 		this.updated = false;
+
+		this.position = Geometry.Vector2.Zero.clone();
 	},
 
 	toMessage: function () {
@@ -20,12 +24,14 @@ var PlayerActor = function (data) {
 
 	this.velocity = 0;
 	this.rotation = 0;
-}
+};
 
 PlayerActor.prototype = Object.create(Actor);
 
 PlayerActor.prototype.toMessage = function () {
 	return {
+		x: this.position.x,
+		y: this.position.y,
 		v: this.velocity,
 		r: this.rotation
 	};
