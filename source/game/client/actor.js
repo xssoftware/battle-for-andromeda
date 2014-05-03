0340f6e3d53fd87ba515f1c12e4138ab0d22432c
Todor Pitekov <esembidi@gmail.com>
2014-05-03 14:48:59 +0300

    actor now keeps update rate and update step; it can now update its position and rotation optionally with animation

--- source/game/client/actor.js
+++ source/game/client/actor.js
@@ -3,10 +3,12 @@ var Actor = {
 		PLAYER: 1
 	},
 
-	_init: function (id, type, entity) {
+	_init: function (id, type, updateRate, entity) {
 		this.id = id;
 		this.type = type;
 		this.entity = entity || new SRA.Entity();
+		this.updateRate = updateRate;
+		this.updateStep = 1.0 / updateRate;
 	},
 
 	update: function (data) {
@@ -14,17 +16,52 @@ var Actor = {
 	}
 }
 
-var PlayerActor = function (id, entity) {
-	this._init(id, Actor.Types.PLAYER, entity);
+var PlayerActor = function (id, updateRate, entity) {
+	this._init(id, Actor.Types.PLAYER, updateRate, entity);
+	this.entity.backgroundColor = Graphics.Color.Yellow;
+	this.lastMoveAction = null;
+	this.lastRotateAction = null;
 }
 
 PlayerActor.prototype = Object.create(Actor);
 
-PlayerActor.prototype.update = function (data) {
-	this.entity.rect.size.width = data.w;
-	this.entity.rect.size.height = data.h;
-	this.entity.setPosition(new Geometry.Vector2(data.x, data.y));
-	this.entity.backgroundColor = Graphics.Color.Yellow;
+PlayerActor.prototype.update = function (data, animate) {
+	if (data.w) {
+		this.entity.rect.size.width = data.w;	
+	}
+	if (data.h) {
+		this.entity.rect.size.height = data.h;
+	}
+
+	var position = new Geometry.Vector2(data.x, data.y);
+
+	if (!animate) {
+		this.entity.setPosition(position);
+		this.entity.rotation = data.r;
+		return;				
+	}
+
+	if (!this.entity.getPosition().equals(position)) {
+		var move = new SRA.MovePositionToAction(position, this.updateStep, 1.0);
+		this.entity.addAction(move);
+
+		if (this.lastMoveAction) {
+			this.lastMoveAction.end(true);
+		}
+
+		this.lastMoveAction = move;
+	}
+	
+	if (!Geometry.isFloatEqualToFloat(this.entity.rotation, data.r)) {
+		var rotate = new SRA.RotateToAction(data.r, this.updateStep, 1.0);
+		this.entity.addAction(rotate);
+
+		if (this.lastRotateAction) {
+			this.lastRotateAction.end(true);
+		}
+
+		this.lastRotateAction = rotate;
+	}
 }
 
 Actor.constructorByType = {
