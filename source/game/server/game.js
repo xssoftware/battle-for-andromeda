4fc99d4147abed83cce85f4c5d4849552f7d88c7
Todor Pitekov <esembidi@gmail.com>
2014-05-04 14:45:56 +0300

    game object now randomly positions players with respect to one another

--- source/game/server/game.js
+++ source/game/server/game.js
@@ -38,24 +38,60 @@ Game.prototype.checkPlayerPlayerCollision = function () {
 	for (var i = 0, length = playerActors.length; i < length; i++) {
 		var a1 = playerActors[i];
 
-		if (!a1.alive || a1.invincible) {
+		if (!playerIsCollidable(a1)) {
 			continue;
 		}
 
-		for (var j = i + 1; j < length; j++) {
-			var a2 = playerActors[j];
+		var a2 = this.collisionActorForPlayer(playerActors, i + 1, a1, playerIsCollidable);
 
-			if (!a2.alive || a2.invincible) {
-				continue;
-			}
-
-			if (a1.polygon.intersects(a2.polygon)) {
-				a1.destroy();
-				a2.destroy();
-				break;
-			}
+		if (a2) {
+			a1.destroy();
+			a2.destroy();
 		}
 	}
+}
+
+Game.prototype.collisionActorForPlayer = function (actors, index, player, validator) {
+	var polygon = player.polygon;
+
+	for (var i = index, l = actors.length; i < l; i++) {
+		var a = actors[i];
+
+		if (validator && !validator(a)) {
+			continue;
+		}
+
+		if (polygon.intersects(a.polygon)) {
+			return a;
+		}
+	}
+
+	return null;
+}
+
+Game.prototype.positionPlayerOnField = function (player) {
+	var startX = player.width / 2.0;
+	var startY = player.height / 2.0;
+	var fieldWidth = this.fieldSize.width - (2.0 * player.width);
+	var fieldHeight = this.fieldSize.height - (2.0 * player.height);
+	var allPlayers = this.server.getActors(Actor.Types.PLAYER);
+	var x, y, r;
+
+	do {
+		x = startX + Math.round(Math.random() * fieldWidth);
+		y = startY + Math.round(Math.random() * fieldHeight);
+		r = Math.random() * (Math.PI * 2.0);
+		player.polygon.transform(x, y, r);
+	} while (this.collisionActorForPlayer(allPlayers, 0, player));
+
+	player.position.x = x;
+	player.position.y = y;
+	player.rotation = r;
+}
+
+// utility
+function playerIsCollidable(player) {
+	return player.alive && !player.invincible;
 }
 
 module.exports = Game;
